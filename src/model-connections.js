2537c3bc6aa6ca98f29302e2dfd422ab4f1334c3
Jatin Kumar <jatinkumaroff@gmail.com>
2026-02-09 23:01:00 +0530

    connection send and review functionalities completed

--- src/model-connections.js
+++ src/model-connections.js
@@ -1,24 +1,41 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connectionSchema= mongoose.Schema({
-   fromUserId:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true
-   },
-   toUserId:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true
-   },
-   status:{
-    type:String,
-    required:true,
-    enum:{
-        values:["interested","ignored","accepted","rejected"],
-        message:"galat status mat daal"
-    }
-   },
+const connectionSchema = mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: "galat status mat daal",
+      },
+    },
+  },
+  { timestamps: true },
+);
 
-},{timestamps:true});
+connectionSchema.pre("save", async function () {
+  const doc = this;
+  //document that is meant to be saved like User({...}) -this is document
+  const connectionPresent = await ConnectionRequest.findOne({
+    $or: [
+      { toUserId: doc.toUserId, fromUserId: doc.fromUserId },
+      { toUserId: doc.fromUserId, fromUserId: doc.toUserId },
+    ],
+  });
 
+  if (connectionPresent) {
+    throw new Error("kitni bar bhejega");
+  }
+});
 
-module.exports = mongoose.model("ConnectionRequest",connectionSchema);
+const ConnectionRequest = mongoose.model("ConnectionRequest", connectionSchema);
+module.exports = ConnectionRequest;
--- src/router-connection.js
+++ src/router-connection.js
@@ -1,37 +1,95 @@
-const express=require('express');
-const connectionRouter=express.Router();
-const ConnectionRequest =require("./model-connections");
-const userAuth=require('./middleware-userAuth')
+const express = require("express");
+const connectionRouter = express.Router();
+const ConnectionRequestModel = require("./model-connections");
+const userAuth = require("./middleware-userAuth");
+const VALID_SEND_STATUS = ["interested", "ignored"];
+const VALID_REVIEW_STATUS = ["accepted", "rejected"];
+const User = require("./model-user");
 
-const VALID_SEND_STATUS=["interested","ignored"]
+//send
+connectionRouter.post(
+  "/connection/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id.toString();
+      const { toUserId, status } = req.params;
 
-connectionRouter.post('/connection/send/:status/:toUserId',userAuth, async (req,res)=>{
-    try{
-        const fromUserId=req.user._id;
-        const {toUserId,status}=req.params;
+      //status check
+      if (!VALID_SEND_STATUS.includes(status)) {
+        throw new Error("tere baap ne ye status banaya h?" + status);
+      }
 
-        const newRequest= new ConnectionRequest({
-            fromUserId,
-            toUserId,
-            status
-        });
+      //user check
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        throw new Error("mann se bana raha hai kya user id?");
+      }
 
-        if(!VALID_SEND_STATUS.includes(status)){
-            throw new Error('tere baap ne ye status banaya h?' + status);
-        }
-        console.log(newRequest);
-        newRequest.save();
-        console.log('request sent by: ' + fromUserId);
-        console.log('request sent to: ' + req.params.toUserId);
-        console.log('request status is: ' + req.params.status);
-        res.send("pohonch gaya idhar ")
-    }catch(err){
-        res.status(400).send("connection save nahi hogi kyuki: " +err.message)
+      //self request check
+      if (toUserId == fromUserId) {
+        throw new Error("bsdk khud ko bhej raha h request?");
+      }
+
+      const newRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      //schema level check
+      await newRequest.save();
+      res.send("request sent");
+    } catch (err) {
+      res.status(400).send("connection save nahi hogi kyuki: " + err.message);
+    }
+  },
+);
+
+//review
+connectionRouter.post("/connection/review/:status/:requestId",userAuth,async (req, res) => {
+  try{
+
+    const { requestId, status } = req.params;
+    const loggedInUserId = req.user._id.toString();
+    
+    //checking if request is present:
+    const connectionRequest = await ConnectionRequestModel.findById(requestId);
+    if(!connectionRequest){
+      throw new Error("aisi koi request hai hi nahi, galat requestId")
     }
 
 
+    //checking if getting reviewd by correct user:
+    if (loggedInUserId != connectionRequest.toUserId.toString()) {
+      throw new Error("apni requests dekh, dusro ki kyu dekh raha");
+    }
 
+    //STATUS IGNORE TO NAHI HAI:
+    if(connectionRequest.status!="interested"){
+      throw new Error("nahi ho payega bhai, status interested ke alawa kuch aur hai")
+    }
 
-})
+    if (!VALID_REVIEW_STATUS.includes(status)) {
+      throw new Error("tere baap ne ye status banaya h?" + status);
+    }
 
-module.exports =connectionRouter;
+    console.log(connectionRequest);
+    const updatedRequest= await ConnectionRequestModel.findOneAndUpdate(
+      {toUserId:loggedInUserId},
+      {$set:{status:status}},
+      {new:true}
+    )
+    console.log(updatedRequest)
+    res.send("updated the status for: "+ loggedInUserId +" to " + status);
+
+    
+  }catch(err){
+    res.status(400).send("connection review me error hai: "+ err.message);
+  }
+  
+
+  
+  },
+);
+module.exports = connectionRouter;
